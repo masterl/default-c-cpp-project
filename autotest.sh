fcf7ea7e898acb5c3d35f05b255a2834d8c814bf
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2017-04-07 18:43:57 -0300

    Further improves automated test script

--- autotest.sh
+++ autotest.sh
@@ -8,8 +8,51 @@ if [ "$CURRENT_ULIMIT_SIZE" -lt 4000 ]; then
   ulimit -Sn 5000
 fi
 
+ignore_list=()
+ignore_list+=('/.git')
+ignore_list+=('/autotest.sh') # You should manually restart this script upon edition
+ignore_list+=('/.gitignore')
+
+prefixed_ignore_list=()
+
+prefix_ignore_list()
+{
+  for element in "${ignore_list[@]}"
+  do
+    prefixed_ignore_list+=("$PROJECT_ROOT$element")
+  done
+}
+
+prefix_ignore_list
+
+grep_ignore_list=''
+
+for element in "${prefixed_ignore_list[@]}"
+do
+  grep_ignore_list="$grep_ignore_list grep -v \"$element\" |"
+done
+
+
 readonly TEST_COMMAND="make -C $PROJECT_ROOT test"
 
+readonly GIT_STATUS_COMMAND="git status"
+readonly PRINT_LINE="echo \\\"==================================================\\\""
+
+entr_command="entr -d sh -c \""
+entr_command="$entr_command tput reset;"
+entr_command="$entr_command echo \\\"Running tests...\\\";"
+entr_command="$entr_command $PRINT_LINE;"
+entr_command="$entr_command $TEST_COMMAND;"
+entr_command="$entr_command echo; $PRINT_LINE;"
+entr_command="$entr_command echo \\\"Running GIT Status...\\\";"
+entr_command="$entr_command $PRINT_LINE;"
+entr_command="$entr_command $GIT_STATUS_COMMAND;"
+entr_command="$entr_command echo;"
+entr_command="$entr_command date;"
+entr_command="$entr_command\""
+
+readonly FULL_COMMAND_STRING="find $PROJECT_ROOT | $grep_ignore_list $entr_command"
+
 while true; do
-  find "$(pwd)" | grep -E ".(cpp|hpp)" | grep -v "/.git/" | entr -d sh -c "tput reset; $TEST_COMMAND;echo;date"
+  eval "$FULL_COMMAND_STRING"
 done
