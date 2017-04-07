cbb1af3966789a93b772700db40b2997fc27c9e4
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2017-04-07 18:24:27 -0300

    Updates ulimit size so entr can track all project files

--- autotest.sh
+++ autotest.sh
@@ -2,6 +2,14 @@
 
 readonly PROJECT_ROOT=$( cd "$( dirname "$0" )" && pwd )
 
+readonly CURRENT_ULIMIT_SIZE=$(ulimit -Sn)
+
+if [ "$CURRENT_ULIMIT_SIZE" -lt 4000 ]; then
+  ulimit -Sn 5000
+fi
+
+readonly TEST_COMMAND="make -C $PROJECT_ROOT test"
+
 while true; do
-  find "$(pwd)" | grep -E ".(cpp|hpp)" | grep -v "/.git/" | entr -d sh -c "tput reset; make -C $PROJECT_ROOT test;echo;date"
+  find "$(pwd)" | grep -E ".(cpp|hpp)" | grep -v "/.git/" | entr -d sh -c "tput reset; $TEST_COMMAND;echo;date"
 done
