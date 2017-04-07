dd08032a2a85d64a919c4896b1e6df05baff3873
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2017-04-07 18:16:51 -0300

    Automated testing script can now be run from other folders

--- autotest.sh
+++ autotest.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+readonly PROJECT_ROOT=$( cd "$( dirname "$0" )" && pwd )
+
 while true; do
-find `pwd` | grep -E ".(cpp|hpp)" | grep -v "/.git/" | entr -d sh -c "tput reset; make test;echo;date"
+  find "$(pwd)" | grep -E ".(cpp|hpp)" | grep -v "/.git/" | entr -d sh -c "tput reset; make -C $PROJECT_ROOT test;echo;date"
 done
