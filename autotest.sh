920c438d92de86912efa56e7f1f67f0944798231
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2018-08-31 16:35:11 -0700

    Huge improvement on autotest.sh
    Removes eval
    Improves file greping

--- autotest.sh
+++ autotest.sh
@@ -3,55 +3,41 @@
 readonly PROJECT_ROOT=$( cd "$( dirname "$0" )" && pwd )
 
 ignore_list=()
-ignore_list+=('/[.]git')
-ignore_list+=('/autotest.sh') # You should manually restart this script upon editing it
-ignore_list+=('/[.]gitignore')
 ignore_list+=('/objs')
-ignore_list+=('/bin')
+ignore_list+=('/.keep')
 
-prefixed_ignore_list=()
+all_files=''
 
-prefix_ignore_list()
+refresh_files_list()
 {
+  all_files=$(find "$PROJECT_ROOT" | grep -E "($PROJECT_ROOT/src)|($PROJECT_ROOT/tests)")
+
   for element in "${ignore_list[@]}"
   do
-    prefixed_ignore_list+=("$PROJECT_ROOT$element")
+    all_files=$(echo "$all_files" | grep -v -E "$element")
   done
 }
 
-prefix_ignore_list
-
-grep_ignore_list=''
-
-for element in "${prefixed_ignore_list[@]}"
-do
-  grep_ignore_list="$grep_ignore_list grep -v \"$element\" |"
-done
-
-
 readonly TEST_COMMAND="make -C $PROJECT_ROOT test"
 
-readonly GIT_STATUS_COMMAND="git status"
-readonly PRINT_LINE="echo \\\"==================================================\\\""
+readonly GIT_STATUS_COMMAND="git -C $PROJECT_ROOT status"
 
-readonly FULL_COMMAND_STRING="find $PROJECT_ROOT |
-grep -E \"($PROJECT_ROOT/src)|($PROJECT_ROOT/tests)\" |
-${grep_ignore_list}
-entr -d bash -c \"
-  tput reset;
-  echo \\\"Running tests...\\\";
+readonly PRINT_LINE="echo \"==================================================\""
+
+readonly COMMAND_STRING="tput reset;
+  echo \"Running tests...\";
   $PRINT_LINE;
   $TEST_COMMAND;
   echo;
   $PRINT_LINE;
-  echo \\\"Running GIT Status...\\\";
+  echo \"Running GIT Status...\";
   $PRINT_LINE;
   $GIT_STATUS_COMMAND;
   echo;
   date;
-  \"
 "
 
 while true; do
-  eval "$FULL_COMMAND_STRING"
+  refresh_files_list
+  echo "${all_files[@]}" | entr -d bash -c "$COMMAND_STRING"
 done
